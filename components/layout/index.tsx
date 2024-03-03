8c02002d0b16c8c3c77ff7583cc67f04c008ebd9
F I R E <firethedev@gmail.com>
2024-03-03 22:11:49 +0600

    meta description updated

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -22,7 +22,7 @@ export default function Layout({
           name="description"
           content={
             description ||
-            "Welcome to Proxy IRAS (IUB), your comprehensive solution for managing academic tasks seamlessly. This platform is integrated with Independent University, Bangladesh's existing management system along with Google Classroom. Log in with your student id and password now. - @firedev99 | iub | proxy iub | iub proxy"
+            "Welcome to Proxy IRAS, your comprehensive solution for managing academic tasks seamlessly. This platform is integrated with Independent University, Bangladesh's existing management system along with Google Classroom. Log in with your student id and password now. - @firedev99 | iub | proxy iub | iub proxy"
           }
         />
         <link rel="icon" href={icon || "/favicon.ico"} />
@@ -38,7 +38,7 @@ export default function Layout({
           property="og:description"
           content={
             description ||
-            "Welcome to Proxy IRAS (IUB), your comprehensive solution for managing academic tasks seamlessly. This platform is integrated with Independent University, Bangladesh's existing management system along with Google Classroom. Log in with your student id and password now. - @firedev99 | iub | proxy iub | iub proxy"
+            "Welcome to Proxy IRAS, your comprehensive solution for managing academic tasks seamlessly. This platform is integrated with Independent University, Bangladesh's existing management system along with Google Classroom. Log in with your student id and password now. - @firedev99 | iub | proxy iub | iub proxy"
           }
         />
         <meta property="og:type" content="website" />
