2af44ac3478beee296eb00b9ee17d7515f8da7f8
F I R E <firethedev@gmail.com>
2024-09-22 22:42:20 +0600

    user issue - offered courses (empty status) fixed - approach 2

--- hooks/useCourses.ts
+++ hooks/useCourses.ts
@@ -1,14 +1,16 @@
+// import { CourseOffering } from "@/types"
 import { CourseOffering } from "@/types"
 import { useQuery } from "@tanstack/react-query"
+
+type IUBCOURSEFETCHRES = { success: boolean; data: CourseOffering[] }
 
 export function useCourses() {
   const { data, isLoading, isError } = useQuery({
     queryKey: ["courses"],
-    queryFn: async () =>
+    queryFn: async (): Promise<IUBCOURSEFETCHRES> =>
       await fetch(`${process.env.NEXT_PUBLIC_URL}/api/iub/courses`)
         .then((response) => response.json())
         .catch((err) => console.log(err)),
-    select: (data) => data as { success: boolean; data: CourseOffering[] },
   })
 
   return {
